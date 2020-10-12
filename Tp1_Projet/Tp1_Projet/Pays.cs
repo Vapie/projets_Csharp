b5448448c995b838be740ab3db49de4b16ef8eda
Valentin Tapie <valentin.tapie@etu.univ-smb.fr>
2020-10-12 10:27:08 +0200

    exercice 7

---
+++ Tp1_Projet/Tp1_Projet/Pays.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1_Projet
+{
+   
+    class Pays
+    {
+         private int id;
+         private string name;
+
+        public Pays(int id, string nom)
+        {
+            this.Id = id;
+            this.Name = nom;
+        }
+
+        public int Id { 
+            get => id;
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentNullException("L'id ne peut pas être inèrieure à 1");
+               
+                this.id= value;
+            }
+        }
+        public string Name { get => name;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("le nom ne peut pas être nul");
+                if (value.Length == 0)
+                    throw new ArgumentException("le prénom ne peut être de longeur 0");
+                this.name = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + "Id = " + Id ;
+        }
+    }
+}
---
+++ Tp1_Projet/Tp1_Projet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp1_Projet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pays> pays = new List<Pays>();
+            try
+            {
+                pays.Add ( new Pays(1, "Fronce"));
+                pays.Add ( new Pays(2, "Almagne"));
+                pays.Add ( new Pays(3, "Itali"));
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+
+            foreach (Pays pay in pays)
+            {
+                Console.WriteLine(pay.ToString());
+            }
+            TdDebug t = new TdDebug();
+            // t.Exercice3();
+            t.Exercice6();
+            Task.Delay(5);
+            
+        }
+    }
+}
